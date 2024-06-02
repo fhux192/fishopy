48376948548c993201b5f722be2e3b7213803713
PeGa CyRus <hoangphuc395@gmail.com>
2024-06-02 10:02:26 +0700

    fix

--- src/components/Layouts/MessageBox.jsx
+++ src/components/Layouts/MessageBox.jsx
@@ -7,7 +7,7 @@ const MessageBox = () => {
       whileHover={{scale:1.5}}
       href="https://www.facebook.com/vduyit"
       target="_blank"
-      className="fixed w-[45px] h-[45px] hover:scale-110 top-[50rem] lg:right-[0.5rem] right-[0.1rem] min-[360px]:top-[40rem] min-[375px]:top-[36rem] min-[410px]:top-[50rem] min-[390px]:top-[46rem] lg:top-[54rem] cursor-pointer z-[22]"
+      className="fixed w-[45px] h-[45px] hover:scale-110 top-[30rem] lg:right-[0.5rem] right-[0.1rem] min-[360px]:top-[20rem] min-[375px]:top-[36rem] min-[410px]:top-[30rem] min-[390px]:top-[26rem] lg:top-[34rem] cursor-pointer z-[22]"
     >
       <div className="rounded-full border-4 ">
       <SiZalo className="w-[35px] h-[35px] text-blue-500" />
