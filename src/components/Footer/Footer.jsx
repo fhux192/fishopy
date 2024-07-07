df50aa97790ab44c16739fda8b82136126ba873a
PeGa CyRus <hoangphuc395@gmail.com>
2024-07-07 09:11:37 +0700

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-primaryTeal ">
       <ItemsContainer />
       <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-2 gap-10  text-center font-mono  text-white text-sm py-3 place-items-center ">
-        <span>© 2020 Appy. All rights reserved.</span>
+        <span>© 2024 Appy. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
       </div>
     </footer>
