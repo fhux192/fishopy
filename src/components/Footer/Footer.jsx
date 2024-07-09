34f3034a0da40991c350cbab3ce6c53dad1916ed
PeGa CyRus <hoangphuc395@gmail.com>
2024-07-09 10:34:52 +0700

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { ItemsContainer } from "./ItemsContainer";
 
 const Footer = () => {
   return (
-    <footer className="bg-primaryTeal  ">
+    <footer className="bg-teal-700  ">
       <ItemsContainer />
       <div className="grid grid-cols-2 font-bold sm:grid-cols-2 lg:grid-cols-2 gap-10  text-center font-mono  text-white text-md py-3 place-items-center ">
         <span>© 2024 Appy. All rights reserved.</span>
--- src/components/Layouts/MessageBox.jsx
+++ src/components/Layouts/MessageBox.jsx
@@ -1,14 +1,32 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { SiZalo } from "react-icons/si";
+import Draggable from "react-draggable";
 
 const MessageBox = () => {
+  const messages = [
+    "Bạn cần giúp gì không?",
+    "Hôm nay của bạn thế nào?",
+    "Đừng ngại hỏi mình nhé!",
+    "Cần hỗ trợ thì nói nhé!",
+    "Bạn dẫn mình đi mua cá đi!"
+  ];
+
   const [showMessage, setShowMessage] = useState(false);
   const [animateWave, setAnimateWave] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [currentMessage, setCurrentMessage] = useState(messages[0]);
+  const [dragMessage, setDragMessage] = useState(false);
+
+  const handleDrag = (e, data) => {
+    setPosition({ x: data.x, y: data.y });
+    setDragMessage(true);
+  };
 
   useEffect(() => {
     const showTimer = setTimeout(() => {
       setShowMessage(true);
+      setCurrentMessage(messages[Math.floor(Math.random() * messages.length)]);
       const hideTimer = setTimeout(() => {
         setShowMessage(false);
       }, 5000);
@@ -33,31 +51,47 @@ const MessageBox = () => {
   }, []);
 
   return (
-    <div className="fixed bottom-[1rem] right-[1rem] z-[22] flex flex-col items-end">
-      {showMessage && (
-        <motion.div
-          initial={{ x: 10 }}
-          animate={{ x: 0 }}
-          transition={{ duration: 1 }}
-          className="bg-white shadow-md shadow-gray-500 rounded-xl rounded-b-none rounded-l-xl p-3 mr-[2.8rem] mb-[0.2rem] text-sm text-gray-800"
-        >
-          Chào bạn! Bạn cần giúp gì không?
-        </motion.div>
-      )}
-      <motion.a
-        whileHover={{ scale: 1.3 }}
-        href="https://zalo.me/0388811160"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={`relative w-[50px] h-[50px] shadow-md shadow-gray-500 rounded-full bg-white cursor-pointer ${
-          animateWave ? "waving-icon" : ""
-        }`}
-      >
-        <div className="rounded-full border-white border-4">
-          <SiZalo className="w-[40px] h-[40px] text-blue-500 animate-pulse" />
+    <div className="fixed bottom-[1rem] right-[1rem] z-[22]">
+      <Draggable onDrag={handleDrag} onStop={() => setDragMessage(false)}>
+        <div className="flex flex-col items-end" style={{ position: 'relative', transform: `translate(${position.x}px, ${position.y}px)` }}>
+          {showMessage && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+              className="bg-white shadow-md shadow-gray-500 rounded-xl rounded-b-none rounded-l-xl p-3 mb-2 text-sm text-gray-800"
+              style={{ position: 'absolute', bottom: '40px', right: '55px' ,width: '200px' ,textAlign: 'center'}}
+            >
+              <p>{currentMessage}</p>
+            </motion.div>
+          )}
+          {dragMessage && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+              className="bg-white shadow-md shadow-gray-500 rounded-xl rounded-b-none rounded-l-xl p-3 mb-2 text-sm text-gray-800"
+              style={{ position: 'absolute', bottom: '40px', right: '55px' ,width: '200px' ,textAlign: 'center'}}
+            >
+              <p>Bạn đưa mình đi đâu vậy?</p>
+            </motion.div>
+          )}
+          <motion.a
+            whileHover={{ scale: 1.3 }}
+            href="https://zalo.me/0388811160"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`relative w-[50px] h-[50px] shadow-md shadow-gray-500 rounded-full bg-white cursor-pointer ${
+              animateWave ? "waving-icon" : ""
+            }`}
+          >
+            <div className="rounded-full border-white border-4">
+              <SiZalo className="w-[40px] h-[40px] text-blue-500 animate-pulse" />
+            </div>
+            <div className="light-rays"></div>
+          </motion.a>
         </div>
-        <div className="light-rays"></div>
-      </motion.a>
+      </Draggable>
       <style>
         {`
           @keyframes wave {
